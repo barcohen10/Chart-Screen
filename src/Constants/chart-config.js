87d3e5847d983e551ba8e4ca9cac4918ae948c1b
barcohen10 <barcohen10@gmail.com>
2019-03-15 11:09:56 +0200

    create a constant file for basic chart config

---
+++ src/Constants/chart-config.js
@@ -0,0 +1,21 @@
+export const DATASET_CONFIG = {
+    datasets: [
+        {
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointHitRadius: 10,
+        },
+    ],
+}
