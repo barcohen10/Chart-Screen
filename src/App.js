09c26914f3f6534b036fa52987c82b46489b4329
barcohen10 <barcohen10@gmail.com>
2019-03-15 11:08:17 +0200

    Create a filter state object for app component + Render Header component and chart with those filters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Components/Header/Header'
+import Chart from './Components/Chart/Chart'
+import { POPULATION_ISRAEL_API as DEFAULT_API } from './Constants/apis';
 
-const App = () => (
-  <Header />
-)
+const App = () => {
+  const [filters, setFilters] = useState({
+    currentAPI: DEFAULT_API,
+    numOfDataPoints: 5,
+    threshold: ''
+  });
+
+  return (<div>
+    <Header filters={filters} setFilters={setFilters} />
+    <Chart filters={filters} />
+  </div>)
+}
 
 export default App;
