d1e0050e253bdac15bd511b48f5576114bd14d05
barcohen10 <barcohen10@gmail.com>
2019-03-15 11:27:16 +0200

    Create a service for israel population API from 2009 to 2019

---
+++ src/Services/israel-population-service.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+function fetchAndCalculateChartData() {
+    this.chartData = {
+        data: [],
+        labels: [],
+    };
+    const promiseArray = []
+    const fetchPopulation = (() => {
+        let year = 2009
+        return () => {
+            this.chartData.labels = [...this.chartData.labels, year]
+            return axios.get(`http://api.population.io/1.0/population/${year++}/Israel?format=json`)
+        }
+    })();
+
+    for (let i = 0; i <= 10; i++) {
+        promiseArray.push(fetchPopulation())
+    }
+
+    return axios.all(promiseArray)
+        .then((responseArray) => {
+            responseArray.forEach(({ data }) => {
+                let population = 0
+                data.forEach(({ total }) => {
+                    population += total
+                });
+                this.chartData.data.push(population)
+            });            
+            return this.chartData
+        });
+}
+
+export default class IsraelPopulationService {
+
+    constructor() {
+        this.chartData = null
+    }
+
+    getChartData() {
+        if (!this.chartData) {
+            return fetchAndCalculateChartData.call(this)
+        }
+        else {
+            return Promise.resolve(this.chartData)
+        }
+    }
+}
